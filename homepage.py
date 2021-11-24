e23e28086c0824fdd21391c9af43c64ff3144d94
walkerfrankenberg <wfrankenberg@middlebury.edu>
2021-11-23 16:27:12 -0800

    continued updates to the website, both aesthetic and funcitonal. also got the current data working

--- homepage.py
+++ homepage.py
@@ -2,29 +2,29 @@ from flask import Flask, render_template
 
 from multipage import MultiPage
 from pages import average_demand #, day_average_demand, real_time, bihall # import your pages here
-#from pages.average_demand import app
+
 
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
-   return render_template('homepage.html')
+   return render_template('homepage.html', title="Welcome to Green Midd")
+
+@app.route('/average_demand')
+def avg_demand():
+   return average_demand.app()
 
 @app.route('/bihall')
 def bihall():
-   return render_template('bihall.html')
-
-@app.route('/test')
-def test():
-   return render_template('average_demand.html', title = "Test")
+   return render_template('bihall.html', title="Bicentennial Hall")
 
 
 
 
 
-@app.route('/chart/')
-def chart():
-   return average_demand.app()
+
+
+
 
 if __name__ == '__main__':
    app.run()
